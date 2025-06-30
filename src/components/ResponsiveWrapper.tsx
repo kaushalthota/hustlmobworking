fce125f5db801323f028c3ce4248880f539312ea
asayini2 <aryansayini.business@gmail.com>
2025-06-30 00:58:00 -0400

    Revert Responsive Mobile Design Changes

--- src/components/ResponsiveWrapper.tsx
+++ src/components/ResponsiveWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState, useEffect } from 'react';
+import React, { ReactNode } from 'react';
 
 interface ResponsiveWrapperProps {
   children: ReactNode;
@@ -9,28 +9,8 @@ const ResponsiveWrapper: React.FC<ResponsiveWrapperProps> = ({
   children, 
   className = '' 
 }) => {
-  const [isMobile, setIsMobile] = useState(false);
-  
-  useEffect(() => {
-    // Check if mobile on mount
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    
-    // Initial check
-    checkMobile();
-    
-    // Add resize listener
-    window.addEventListener('resize', checkMobile);
-    
-    // Clean up
-    return () => {
-      window.removeEventListener('resize', checkMobile);
-    };
-  }, []);
-  
   return (
-    <div className={`${isMobile ? 'main-content pb-mobile-nav' : ''} ${className}`}>
+    <div className={className}>
       {children}
     </div>
   );

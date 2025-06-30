a6c8e095f3400c18f81cbee7f583f12a1458d504
Ashray-tech <111703461+Ashray-tech@users.noreply.github.com>
2025-06-30 15:38:57 -0400

    Updated App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -389,6 +389,7 @@ return (
       {/* ...rest of your JSX as before (not modified here for brevity) */}
     </div>
   );
+};
   const handleSignIn = () => {
     setAuthMode('signin');
     setShowAuth(true);
